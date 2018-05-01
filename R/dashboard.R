df094cbed3da9013b0a260ff7a3955ddae296952
Jim Hester <james.f.hester@gmail.com>
2018-05-01 11:16:58 -0400

    Move helper functions to R/

--- R/dashboard.R
+++ R/dashboard.R
@@ -62,6 +62,7 @@ parse_issues_repository <- function(x) {
     mutate(owner = x$owner$login, repo = x$repo) %>%
     select(owner, repo, everything())
 }
+utils::globalVariables("owner")
 
 #' Compute an organization summary
 #' @param org A GitHub organization name
@@ -74,3 +75,46 @@ org_data <- function(org) {
   list(summary = summary, issues = issues)
 }
 
+
+#' Calculate number or reverse dependencies
+#' @param package One or more packages to search
+#' @importFrom purrr map_int
+#' @export
+reverse_dependencies <- function(package) {
+  res <- tools::package_dependencies(package,
+    reverse = TRUE,
+    recursive = FALSE,
+    which = c("Depends", "Imports", "LinkingTo", "Suggests", "Enhances"))
+
+  map_int(res, ~ if (is.null(.x)) NA_integer_ else length(.x))
+}
+
+
+#' Lookup weekly commit counts for each repo
+#' @inheritParams org_data
+#' @inheritParams reverse_dependencies
+#' @export
+weekly_commits <- function(org, package) {
+  tryCatch(
+    map_int(gh::gh("/repos/:org/:package/stats/commit_activity", org = org, package = package), "total"),
+    error = function(e) {
+      NA_integer_
+    })
+}
+
+#' Number of downloads from RStudio mirror
+#' @inheritParams cranlogs::cran_downloads
+#' @inheritParams reverse_dependencies
+#' @importFrom dplyr group_by tally left_join
+#' @export
+num_downloads <- function(package, when = "last-week") {
+  # only lookup packages that are valid package names
+  valid_pkgs <- grepl(paste0("^", .standard_regexps()$valid_package_name, "$"), package)
+  res <- cranlogs::cran_downloads(package[valid_pkgs], "last-week")
+  count_per_package <- res %>% group_by(package) %>% tally(wt = count)
+  out <- numeric(length(package))
+  out[valid_pkgs] <- left_join(tibble(package = package[valid_pkgs]), count_per_package)$n
+  out
+}
+
+utils::globalVariables("count")
