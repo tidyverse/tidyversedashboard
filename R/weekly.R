db724ce89a48b30eaa08b1580fda72900313e3df
Jim Hester <james.f.hester@gmail.com>
2018-05-02 11:10:00 -0400

    Add code for returning weekly issues

---
+++ R/weekly.R
@@ -0,0 +1,52 @@
+dfs_val <- function(x, f) {
+  idx <- dfs_idx(x, f)
+  if (length(idx) == 0) {
+    NULL
+  } else {
+    x[[idx]]
+  }
+}
+
+dfs_idx <- function(x, f) {
+  res <- integer()
+  walk <- function(x, depth) {
+    for (i in seq_along(x)) {
+      if (isTRUE(f(x[[i]]))) {
+        res[[depth]] <<- i
+        return(TRUE)
+      } else {
+        if (is.list(x[[i]]) && isTRUE(walk(x[[i]], depth + 1))) {
+          res[[depth]] <<- i
+          return(TRUE)
+        }
+      }
+    }
+  }
+  walk(x, 1)
+  res
+}
+
+end_cursor <- function(x) is.list(x) && !is.null(x$endCursor)
+
+paginate <- function(f, ...) {
+  out <- append(list(), f(cursor, ...))
+  cursor <- dfs_val(res, end_cursor)[[1]]
+  while (!is.null(cursor)) {
+    res <- f(cursor, ...)
+    out <- append(out, res)
+    cursor <- dfs_val(res, end_cursor)[[1]]
+  }
+  out
+}
+
+parse_weekly_issues <- function(x) {
+  tibble::tibble(package = x$repository$name, type = x$type, number = x$number, closed = as_datetime(x$closedAt %||% NA), opened = as_datetime(x$createdAt %||% NA))
+}
+
+#' @importFrom lubridate today dweeks
+weekly_issues <- function(org, start = today() - dweeks(1)) {
+  res <- paginate(function(cursor, ...) {
+    graphql_query("weekly_issues.graphql", query = glue::glue("org:{org} updated:>={start} sort:updated-dsc"), cursor = cursor)
+  })
+  map_dfr(res, function(x) map_dfr(x$search$nodes, parse_weekly_issues))
+}
