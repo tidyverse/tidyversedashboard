96c8c297eb46c78c4e53396f45ad9aadb36d2ced
Jim Hester <james.f.hester@gmail.com>
2018-05-02 16:18:35 -0400

    Add valueBox's and improve displays

--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,19 @@ graphql_query <- function(json, ...) {
   query <- readChar(file, file.info(file)$size)
   gh::gh("POST /graphql", query = query, variables = compact(list(...)))
 }
+
+#' A wrapper around [DT::datatable] to change some defaults
+#' @importFrom DT datatable
+#' @inheritParams DT::datatable
+#' @export
+# A datatable with some common options set
+data_table <- function(data, options = list(), ..., filter = "top", style = "bootstrap", autoHideNavigation = TRUE, rownames = FALSE) {
+    default_opts <- list(pageLength = 25,
+      dom = "tip",
+      columnDefs = list(
+        list(targets = "_all", orderSequence = c("desc", "asc"))))
+
+    options <- modifyList(default_opts, list())
+
+  datatable(data, ..., options = options, filter = filter, style = style, autoHideNavigation = autoHideNavigation, rownames = rownames)
+}
