e805d4c84b44ce2a9803b51f782c14fe8f234ef6
Jim Hester <james.f.hester@gmail.com>
2018-05-07 16:14:55 -0400

    fixup! fixup! Use faster datetime parser

--- R/pullrequests.R
+++ R/pullrequests.R
@@ -1,22 +1,22 @@
 parse_pr_comments <- function(x) {
   tibble(commenter = x$author$login %||% NA_character_,
-         commented = x$publishedAt %||% NA_character_)
+         commented = parse_datetime_8601(x$publishedAt %||% NA_character_))
 }
 
 parse_pr_reviews <- function(x) {
   tibble(reviewer = x$author$login %||% NA_character_,
-         reviewed = x$submittedAt %||% NA_character_)
+         reviewed = parse_datetime_8601(x$submittedAt %||% NA_character_))
 }
 
 parse_pr_prs <- function(x) {
-  reviews <- map_dfr(x$reviews$nodes, parse_pr_reviews) %|||% tibble(reviewer = character(), reviewed = character())
-  comments <- map_dfr(x$comments$nodes, parse_pr_comments) %|||% tibble(commenter = character(), commented = character())
+  reviews <- map_dfr(x$reviews$nodes, parse_pr_reviews) %|||% tibble(reviewer = character(), reviewed = parse_datetime_8601(character()))
+  comments <- map_dfr(x$comments$nodes, parse_pr_comments) %|||% tibble(commenter = character(), commented = parse_datetime_8601(character()))
 
   tibble(
     number = x$number,
     author = x$author$login %||% NA_character_,
-    created = x$createdAt %||% NA_character_,
-    updated = x$updatedAt %||% NA_character_,
+    created = parse_datetime_8601(x$createdAt %||% NA_character_),
+    updated = parse_datetime_8601(x$updatedAt %||% NA_character_),
     reviews = list(reviews),
     comments = list(comments))
 }
@@ -45,9 +45,5 @@ org_pr <- function(org) {
   
   mutate(
     map_dfr(res, function(x) map_dfr(x$repositoryOwner$repositories$nodes, parse_pr_repository)),
-    created = parse_datetime_8601(created),
-    updated = parse_datetime_8601(updated),
-    reviewed = parse_datetime_8601(reviewed),
-    commented = parse_datetime_8601(commented),
     owner = org)
 }
